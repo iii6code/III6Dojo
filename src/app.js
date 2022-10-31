2c995133b96a3839d998222249e946a626550401
stereoIII6 <stereodocbush@gmail.com>
2022-10-31 17:54:44 +0100

    Update app.js
    redir update

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ import { logo, wallet, mwallet, btn, mbtn, app, mapp, net, mnet, about, mabout, 
 import { mob_toggle, toggle, a, showAdmin, fadeAdmin, doAdmin, openLanding, openWallet, openApp, openNet, openAbout, openService, openTeam, openImprint, openTerms, openContact } from "./nav";
 import { roll, makeId, doCollection, showRarity } from "./rarity";
 const redir = "https://main--admiring-hawking-10a310.netlify.app/";
-window.location.assign(redir);
+window.location.assign(redir); // 
 
 // globals
 let accounts;
