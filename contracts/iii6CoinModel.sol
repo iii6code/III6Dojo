2af6bf109c95070310800332d2120f52e7143b55
stereoIII6 <stereodocbush@gmail.com>
2022-12-02 23:01:02 +0100

    sunny

---
+++ contracts/iii6CoinModel.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-3.0
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//
+//      MMWKd:..                                                                                    ..:dKWMM
+//      MKl.                                                                                            .lKW
+//      O'       ..''''''''''''.     .''''''.     .'''''.      .''''''''''''''.     .''''''''''''..       'O
+//      '     .ckKNNWNWWWWWWWWWk.   .xNWWNWNl    ,0WWWWW0,     lNWWWWWWWWWNWNk'    ;0NNNWWWWWWWWNNKkc.     '
+//           ,OWMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMMMMMMM0,    ;KMMMMMMMMMMMMMMMMWO,
+//          .kMMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMMMMMM0,    ;KMMMMMMMMMMMMMMMMMMMk.
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMMMMM0,    :KMMMMMMMMMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMMMM0,    :KMMMMMMMMMMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMMM0,    ;0NNWWMMMMMMMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMM0,     .'.',;lkNMMMMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMMK;              ,kWMMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMMX:    .:oxxdc'    .dWMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMWd.   ,OWMMMMMXl.   '0MMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMX;   .kMMMMMMMMK,   .xMMMMMMMMMMMM0'
+//          '0MMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMM0'   .OMMMMMMMMX;   .xMMMMMMMMMMMM0'
+//          .OMMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMK,    lNMMMMMMWx.   '0MMMMMMMMMMMMO.
+//           lNMMMMMMMMMMMMMMMMO.   .kMMMMMWl    ,KMMMMMK,     lWMMMWd.   .:x0KKKkc.   .dWMMMMMMMMMMMNl
+//      .    .c0WMMMMMMMMMMMMMMO.   .kMMMMMWo    ,KMMMMMK,     lWMMMMNd.     ....     .xNMMMMMMMMMMW0c.    .
+//      l      .,lddxxxxxxxxxxxc.    :xxxxxd,    .lxxxxxl.     ,dxxxxxd:.            .cdxxxxxxxxddl,.      l
+//      No.                                                                                              .oN
+//      MW0l'                                                                                          'l0WM
+//      MMMWKd;.                                                                                    .;dKWMMM
+//
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @dev            ::              Aron Mauritala Ayuk                                                                                                                                          //
+//      @msg            ::              stereo@iii6.xyz                                                                                                                                   //
+//      @github         ::              @stereoIII6
+//      @twitter        ::              @stereoIII6                                                                                                                                              //
+//                                                                                                                                                                                  //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @dev            ::              Juan Xavier Valverde                                                                                                                                    //
+//      @msg            ::              juanxavier@iii6.xyz                                                                                                                               //
+//      @twitter        ::              @JuanXavier                                                                                                                                             //
+//      @github         ::              @JuanXavier                                                                                                                                             //
+//                                                                                                                                                                                  //                                                                                                                                                                                 //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @company        ::              Fractio Holding                                                                                                                                                                       //
+//      @title          ::              iii6 Coin Model                                                                                                                            //
+//      @description    ::              ERC20 Model Preset Contract                                                                                                                           //
+//      @version        ::              0.0.1                                                                                                                                       //
+//      @purpose        ::              ERC20 Model Preset                                                                                                          //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
---
+++ contracts/s0xGroups.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GPL-3.0
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//
+//      0KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK0Okdlc:;,......    ......',;cldxO0KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
+//      XNNNNNNNNNNNNNNNNNNNNNNNNNNXX0kdc;'...        .......         ...';coxOKXNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNNNNNNX0xl;'..              .,;;:,.                ..;cdOXXNNNNNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNXNXOo:'.                   .......                    ..;lkKXNNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNXOo;..              .........................             ..,lkKNNNNXNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNXXNKx:..            ..........    'lc'.    ............            .;d0XNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNX0d;.          ........         .;dddkl.            .......          .'lOXNNNNNNNNNNNNN
+//      XNNNNNNNNNNX0d,.         ......             .cx:..,lo'               ......         .'lOXNNNNNNNNNNN
+//      XNNNNXNNNNKx,.         .....               .ox;.   .:o,.                 ....         .'o0XNNNNNNNNN
+//      XNXNNNNNXOc.        .....                .,dd'      .;l;.                  .....   .... .;xXNNNNNNNN
+//      XNNNNNNKd'....... .....                 .;xo.        .'c;.                    .....':;..  .l0XNNNNNN
+//      XNNNNX0c. ..'''......                  .ckc.           .:;.                     ....',...  .;OXNNNNN
+//      XNNNXO:.   ........                   .ok:.             .;;.                     ......     .,xXNNXX
+//      XNNXO;.       ...                    'dx;.               .,;.                      ...        'xXNNN
+//      XNXO;.       ...                   .,xx'       .....''''''';:..                     ...        'xXNN
+//      XN0:.       ...                   .;kd.  ..,:ldxkOOOOOOOOOkO0Odl:,..                  ...      .,kXN
+//      XKl.       ...                   .cko'':ok00Okdlc;;,'''...',;:dkxkkd:..                ..       .:0N
+//      Xx'       ...                   .lKOxk00xxxl,.  ....',..      .'..,cddl,.              ...       .oX
+//      O:.      ...                  ..dNN0xl;':c::c:'..':odol:..     .... ..:lc,.             ...       ,k
+//      d.       ..                 .,o00d:.. .od'  .,cclxxl'.'coc.     ....   ..,,'..           ..       .l
+//      :.      ...              ..:xKNO,.   .lO:    .;k0o;.   .'do.     ...      .....          ...       ,
+//      '       ..             ..cddx0x'     .xO,   .:dc',:;'.   ;k:.           ..,'...           ..       .
+//      .       ..            .:lc,ckd.      .dKc. .cl'.  ..,'. .lO:       ..':ll:,..             ...      .
+//      .      ...          .,:,..ckl.       .;0O:,:,.      ...'lOl.   ..;ldkkdo;.                ...      .
+//             ...        ..'.. .lkc.         .;kK0o,.......':dOx;..,cdOKKko;..'c:.               ...
+//      .      ...       ....  .lk:.           .'coxOkkxxxxkkkxdodkKXXOdc'.    .'dd,.             ...
+//      .      ...        ....,dk;.            ..  ..;coxkkOO0KXX0ko:'..         'dOc.            ...      .
+//      .       ..          .,dd;,,;;:::;;;;;::clloxkO0KK00Oxdl;'..               .l0d'.          ...      .
+//      ,.      ...        .;dc.  ...,;:clloodddddoolc:;'....                      .;OO:.         ......   .
+//      c.  .......       .:o;.                                                     .'d0d'.      ....,;'. .:
+//      x'  .';,...      .cl'.                                                        .:OO:.     ....;;.. .o
+//      0l. ..,'....   .'ol...............................'''',,,,,,;;;:::::ccccclllllookNXd.   ... .... .;0
+//      XO,       ...  .coc::cccccccccllllllllllllloooooooooooooooooooooooooooooooolllllllc;.  ...       .dX
+//      XXd.       ...  .........................................                             ...       .lKN
+//      XNKl.       ...            .:cccccc:;:ccccccc:::cccccccccccccccccccccccc'.           ...       .:0NX
+//      XNNKc.       ...          .cXWNXXNKkxokXNWWNNK0NMWWNXNW0OXWNNWWNWWNWWNWWo.          ...       .;OXNN
+//      XNNN0l.       ....        .cXNxokKxokllOkdxKNOxXWkloddOdlOXKkK0ONWOdx0WWo.         ...       .;OXNNN
+//      XNNNNKo.        ...       .cXNOdokxcddd0klo0WKdOXd;lxkKOd0NWOlokKXOllOWWo.       ...        .c0NNNNN
+//      XNNNNNXd'.       ....     .cXWK00XNKO0XXKNXKWN0KNNK00KNNK0XXOdOKKXKNXKNWo.     ....        .lKNNNNNN
+//      XNNNNNNXO:.        ....    'oddddddddddddddddddddddddddddddl:coddddddddo;.   ....        .,xXNNNNNNN
+//      XNNNNNNNNKd'.        .....                                                 ....        ..l0XNNNNNNNN
+//      XNNNNNNNNNXOl..        ......                                           .....         .:kXNNNNNNNNNN
+//      XNNNNNNNNNNNXOc..         ......                                    ......          .:xKNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNXOl'.          ........                          ........          ..ckKNNNNNNXNNNNNNN
+//      XNNNNNNNNNNNNNNNX0d;..       ..'....................................',,.       .,oOXNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNXOo;..    ...;;.     ....................      .;,'..   ..,lkKXNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNNNNKOd:'.    ..                                 ...  ..;lkKXNXNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNNNNNNNX0ko:,...                                 ..':lx0XNNNNNXNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNNNNNNNNNNNXKOxoc;,....                  ....';:ldOKXXNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//      XNNNNNNNNNNNNNNNNNNNNNNNNNNNXXNNNNXK0kdl:;'.....    .....',:ldk0KXNNNNNXNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @dev            ::              Aron Mauritala Ayuk                                                                                                                                          //
+//      @msg            ::              stereo@iii6.xyz                                                                                                                                   //
+//      @github         ::              @stereoIII6
+//      @twitter        ::              @stereoIII6                                                                                                                                              //
+//                                                                                                                                                                                  //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @dev            ::              Juan Xavier Valverde                                                                                                                                    //
+//      @msg            ::              juanxavier@iii6.xyz                                                                                                                               //
+//      @twitter        ::              @JuanXavier                                                                                                                                             //
+//      @github         ::              @JuanXavier                                                                                                                                             //
+//                                                                                                                                                                                  //                                                                                                                                                                                 //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//                                                                                                                                                                                  //
+//      @company        ::              Fractio Holding                                                                                                                                                                       //
+//      @title          ::              s0xiety Groups                                                                                                                            //
+//      @description    ::              Group Interface Contract                                                                                                                           //
+//      @version        ::              0.0.1                                                                                                                                       //
+//      @purpose        ::              Multinet Group Interface                                                                                                           //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+//                                                                                                                                                                                  //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+//  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   //
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
