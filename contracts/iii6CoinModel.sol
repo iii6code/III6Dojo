df5ba0cc71656a26325edc16fe648c093e3b2f40
stereoIII6 <stereodocbush@gmail.com>
2022-12-03 00:09:22 +0100

    sunny

--- contracts/iii6CoinModel.sol
+++ contracts/iii6CoinModel.sol
@@ -73,3 +73,200 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "./iii6PriceConsumer.sol";
+import "./iii6Math.sol";
+import "./iii6safes.sol";
+
+library MLQlib {
+    // MLQ Token Extension
+    uint256 public constant MLQ_RATE = 100;
+    uint256 public constant MLQ_DIG = 10**18;
+    uint256 public constant MAX_SUPPLY = 3 * 10**9 * MLQ_DIG;
+    uint256 public constant PUB_SUPPLY = 1 * 10**9 * MLQ_DIG;
+    uint256 public constant POOL_RESERVE = 1 * 10**9 * MLQ_DIG;
+    uint256 public constant CIRC_RESERVE = 1 * 10**9 * MLQ_DIG;
+}
+
+contract USDC is ERC20("US Dollar Coin", "USDC") {
+    function dropUSDC() external {
+        _mint(msg.sender, 100 * 10**18);
+    }
+}
+
+contract COIN is ERC20, iii6Math {
+    using MLQlib for *;
+
+    address private admin; // @audit. declare as internal since there are getter functions
+    uint256 public rate; // @audit. declare as internal since there are getter functions
+    uint256 public PUB_SUPPLY; // @audit. declare as internal since there are getter functions
+    uint256 public MAX_SUPPLY; // @audit. declare as internal since there are getter functions
+    string public title;
+
+    mapping(address => uint256) public coinBalance;
+
+    constructor(
+        string memory _name,
+        string memory _sym,
+        uint256 _rate,
+        uint256 _supply,
+        address _admin
+    ) ERC20(_name, _sym) {
+        admin = _admin;
+        setRate(_rate);
+        MAX_SUPPLY = _supply;
+        title = _name;
+    }
+
+    function setRate(uint256 _newRate) internal returns (uint256) {
+        rate = _newRate;
+        return rate;
+    }
+
+    function getRate() external view returns (uint256) {
+        return rate;
+    }
+
+    function getMax() external view returns (uint256) {
+        return MAX_SUPPLY;
+    }
+
+    function getName() external view returns (string memory) {
+        return title;
+    }
+
+    function getMinted() external view returns (uint256) {
+        return PUB_SUPPLY;
+    }
+
+    function buyMlq(uint256 _amnt) external payable returns (uint256) {
+        uint256 amnt = MLQlib.MLQ_DIG * _amnt;
+        if (PUB_SUPPLY < amnt) revert();
+        if (divide(msg.value, 100) < amnt) revert();
+        _mint(msg.sender, amnt);
+        coinBalance[msg.sender] = amnt;
+        PUB_SUPPLY += amnt;
+        return amnt;
+    }
+
+    function coinBal(address _adr) external view returns (uint256) {
+        return coinBalance[_adr];
+    }
+
+    function withdraw(uint256 _amnt) external returns (uint256) {
+        uint256 amnt = MLQlib.MLQ_DIG * _amnt;
+        if (msg.sender != admin) revert();
+        if (coinBalance[address(this)] < amnt) revert();
+        transferFrom(address(this), admin, amnt);
+        return amnt;
+    }
+
+    function flush() external returns (uint256) {
+        if (msg.sender != admin) revert();
+        transferFrom(address(this), admin, coinBalance[address(this)]);
+        payable(admin).transfer(address(this).balance);
+        return address(this).balance;
+    }
+}
+
+// 0 Fuji // 1 Avax // 2 Fantom Test // 4 Fantom Main // 5 Polygon Mumbai // 6 Polygon Main
+contract MLQ is ERC20, iii6Math, iii6Safes {
+    using MLQlib for *;
+
+    address private admin;
+    int256 public rate;
+    uint256 public PUB_SUPPLY;
+    uint256 MLQ_RATE;
+    uint256 mainRate;
+    iii6PriceConsumer public ethUsdPrice;
+    mapping(address => uint256) public mlqBalance;
+    USDC usdc;
+
+    error InsufficientBalance();
+    error Unathorized();
+    error InsufficientMLQ();
+
+    constructor(
+        address _usdc,
+        address _pc,
+        uint256 _net
+    ) ERC20("Milquidity Token", "MLQ") iii6Safes(_net) {
+        admin = msg.sender;
+        usdc = USDC(_usdc);
+        setSupply(MLQlib.PUB_SUPPLY);
+        ethUsdPrice = iii6PriceConsumer(_pc);
+    }
+
+    function setMLQRate() internal returns (int256) {
+        int256 price = ethUsdPrice.EthUsdPrice();
+        uint256 newRate = divide(uint256(price), 100);
+        if (rate != 0) rate = int256(newRate);
+        return rate;
+    }
+
+    function setMainRate() internal returns (int256) {
+        int256 price = ethUsdPrice.MainUsdPrice();
+        uint256 newRate = divide(uint256(price), 100);
+        if (rate != 0) rate = int256(newRate);
+        return rate;
+    }
+
+    function setSupply(uint256 _newSupply) internal returns (uint256) {
+        PUB_SUPPLY = _newSupply;
+        return PUB_SUPPLY;
+    }
+
+    function buyMlqMain() external payable returns (uint256) {
+        setMLQRate();
+        setMainRate();
+        uint256 swapRate = divide(mainRate, MLQ_RATE);
+        uint256 amnt = msg.value * swapRate;
+        if (amnt <= address(this).balance) revert InsufficientBalance();
+
+        // find maincurr price for mlq
+        if (PUB_SUPPLY < amnt) revert();
+        _mint(msg.sender, amnt);
+        mlqBalance[msg.sender] += amnt;
+        PUB_SUPPLY -= amnt;
+        return amnt;
+    }
+
+    function buyMlqUSDC(uint256 _amnt) external payable returns (uint256) {
+        setMLQRate();
+        uint256 amnt = _amnt * uint256(rate) * 10**10;
+        if (PUB_SUPPLY < amnt) revert();
+        usdc.transferFrom(msg.sender, address(this), amnt);
+        _mint(msg.sender, _amnt * 10**18);
+        mlqBalance[msg.sender] = _amnt * 10**18;
+        PUB_SUPPLY -= amnt;
+        return amnt;
+    }
+
+    function mlqBal(address _adr) external view returns (uint256) {
+        return mlqBalance[_adr];
+    }
+
+    function withdraw(uint256 _amnt) external returns (uint256) {
+        if (msg.sender != admin) revert Unathorized();
+        uint256 amnt = MLQlib.MLQ_DIG * _amnt;
+        if (mlqBalance[address(this)] < amnt) revert InsufficientMLQ();
+        transferFrom(address(this), admin, amnt);
+        return amnt;
+    }
+
+    function flush() external returns (uint256) {
+        if (msg.sender != admin) revert Unathorized();
+        transferFrom(address(this), admin, mlqBalance[address(this)]);
+        payable(admin).transfer(address(this).balance);
+        usdc.transferFrom(address(this), admin, usdc.balanceOf(address(this)));
+        return address(this).balance;
+    }
+
+    receive() external payable {
+        uint256 val = divide(msg.value, 1000);
+        payable(iii6).transfer(val * 10);
+        payable(dias).transfer(val * 10);
+        payable(dojo).transfer(val * 10);
+        payable(s0x).transfer(val * 10);
+        payable(codebender).transfer(val * 900);
+    }
+}
