799119fac5084f66d90f0ae2e9f60dcc9085a74c
stereoIII6 <stereodocbush@gmail.com>
2022-12-14 16:05:28 +0100

    sunny

--- contracts/iii6Utils/Assets/iii6AssetFactory.sol
+++ contracts/iii6Utils/Assets/iii6AssetFactory.sol
@@ -78,7 +78,12 @@ contract iii6AssetFactory is iii6DiaModel, iii6PriceMath {
     // ╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
     address VRF;
     address FEED;
-    uint256 licenseFee;
+    uint256 public licenseFee;
+    mapping(address => uint256) public licenseRange;
+    uint256 public p;
+    mapping(uint256 => address) public projects;
+    mapping(address => uint256) public mp;
+    mapping(address => mapping(uint256 => address)) public myProjects;
 
     /*
      * @dev this constructor creates a Dia NFT Token Contract
@@ -101,7 +106,19 @@ contract iii6AssetFactory is iii6DiaModel, iii6PriceMath {
     ) iii6DiaModel(msg.sender, _name, _sym, 0, 0, "", "", false, 0, 0, 0) {}
 
     // set the value of fee for asset creation
-    function _setLicenseFee(uint256 _fee) internal returns (uint256) {}
+    function _setLicenseFee(uint256 _fee) internal returns (uint256) {
+        return licenseFee = _fee * 10**16; // 100 = 1 ETH
+    }
+
+    function _setLicense(address _adr, uint256 _years)
+        internal
+        returns (uint256)
+    {
+        if (licenseRange[_adr] > block.timestamp) {
+            uint256 dif = licenseRange[_adr] - block.timestamp;
+            return licenseRange[_adr] += dif + 60 * 60 * 24 * 365 * _years;
+        } else return licenseRange[_adr] += 60 * 60 * 24 * 365 * _years;
+    }
 
     // function creates a ERC20 Token Contract
     function _makeERC20Asset(
@@ -139,6 +156,48 @@ contract iii6AssetFactory is iii6DiaModel, iii6PriceMath {
         return address(token);
     }
 
+    // function creates a ERC721 Token Contract
+    function _makeERC721Asset(
+        string memory _name,
+        uint256 _max,
+        uint256 _price,
+        uint256 _g
+    ) internal returns (address) {
+        // chose token model
+        iii6DiaModel diaProject;
+        /**
+         * @dev this constructor creates a Dia NFT Token Contract
+         * @param _owner contract admin
+         * @param _name dia token name
+         * @param _max amount of tokens 0 = infinte
+         * @param _price amount of gascoin for Dia token
+         * @param _cName name of related ERC20 token
+         * @param _cSym symbol of related ERC20 token
+         * @param _c bool if related coin exists
+         * @param _cs max supply of related ERC20 token 0 = infinite
+         * @param _rate price of related ERC20 token in gascoin
+         * @param _g number of greenlist slots for Dia NFT project 0 = no greenlist
+         */
+        diaToken = new iii6DiaModel(
+            msg.sender,
+            _name,
+            _max,
+            _price,
+            "",
+            "",
+            false,
+            0,
+            0,
+            _g
+        );
+        return address(diaToken);
+    }
+
+    function buyLicense(uint256 _years) external returns (uint256) {
+        if (msg.value < _years * licenseFee) revert Insufficient_Funds();
+        return _setLicense(msg.sender, _years);
+    }
+
     /**
      * @dev creates an instance of iii6CoinModel
      * @param _name token name
@@ -150,9 +209,9 @@ contract iii6AssetFactory is iii6DiaModel, iii6PriceMath {
      * @param _curr denomintaor currency // 0-eth 1-gascoin 2-xCur 2-yCur >4-usdc
      * @return contract address
      */
-    function buildCoinProject(
-        string memory _name,
-        string memory _sym,
+    function buildERC20Token(
+        string calldata _name,
+        string calldata _sym,
         uint256 _rate,
         uint256 _supply,
         bool _burn,
@@ -160,18 +219,36 @@ contract iii6AssetFactory is iii6DiaModel, iii6PriceMath {
         uint256 _curr
     ) external payable returns (address) {
         // check msg.value
-
-        // check token balance
-        // do payment
+        if (msg.value < licenseFee / 10 || licenseRange > block.timestamp)
+            revert Insufficient_Funds();
         // mint license
         // make asset & exit with address
         return
             _makeERC20Asset(_name, _sym, _rate, _supply, _burn, _pause, _curr);
     }
 
-    // function creates a ERC721 Token Contract
-    function _makeERC721Asset() internal returns (bool) {}
+    /**
+     * @dev function
+     * @param _owner contract admin
+     * @param _name dia token name
+     * @param _max amount of tokens 0 = infinte
+     * @param _price amount of gascoin for Dia token
+     * @param _cName name of related ERC20 token
+     * @param _cSym symbol of related ERC20 token
+     * @param _c bool if related coin exists
+     * @param _cs max supply of related ERC20 token 0 = infinite
+     * @param _rate price of related ERC20 token in gascoin
+     * @param _g number of greenlist slots for Dia NFT project 0 = no greenlist
+     */
+    function buildERC721Token(
+        string calldata _name,
+        uint256 _max,
+        uint256 _price,
+        uint256 _g
+    ) external payable returns (address) {
+        if (msg.value < licenseFee / 5 || licenseRange > block.timestamp)
+            revert Insufficient_Funds();
 
-    // function creates a ERC1155 Token Contract
-    function _makeERC1155Asset() internal returns (bool) {}
+        return _makeERC721Asset(_name, _max, _price, _g);
+    }
 }
